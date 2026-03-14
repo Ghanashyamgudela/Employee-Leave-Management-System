966bb79923cb2bb94b0fae89074d16bc89273f2c
Ghanashyamgudela <157698137+Ghanashyamgudela@users.noreply.github.com>
2026-03-14 16:23:34 +0530

    Change getenv to environ.get for environment variables

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
-MYSQL_HOST = os.getenv("MYSQLHOST")
-MYSQL_USER = os.getenv("MYSQLUSER")
-MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
-MYSQL_DB = os.getenv("MYSQLDATABASE")
-MYSQL_PORT = int(os.getenv("MYSQLPORT", 3306))# your database name
+
+MYSQL_HOST = os.environ.get("MYSQLHOST")
+MYSQL_USER = os.environ.get("MYSQLUSER")
+MYSQL_PASSWORD = os.environ.get("MYSQLPASSWORD")
+MYSQL_DB = os.environ.get("MYSQLDATABASE")
+MYSQL_PORT = int(os.environ.get("MYSQLPORT", 3306))# your database name
 MAIL_USER = "ghana19183@gmail.com"
 MAIL_PASS = "jmwe kafu lmdp cmpr"   # Use Gmail App Password (NOT your Gmail login)
 
