74f975c4d69b676fdfd123cdfcd203093fc7d040
Ghanashyamgudela <157698137+Ghanashyamgudela@users.noreply.github.com>
2026-03-14 15:40:18 +0530

    Refactor MySQL config to use environment variables
    Refactored MySQL configuration to use environment variables for better security and flexibility.

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-# config.py
+import os
 
-MYSQLHOST = 'mysql.railway.internal'       # your MySQL host, usually localhost
-MYSQLUSER = 'root'            # your MySQL username
-MYSQLPASSWORD = 'cxVIqcOfPKWrUPZVknOCkOneoyjMkEDF'            # your MySQL password
-MYSQLDATABASE = 'railway'  # your database name
+MYSQL_HOST = os.getenv("MYSQLHOST")
+MYSQL_USER = os.getenv("MYSQLUSER")
+MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
+MYSQL_DB = os.getenv("MYSQLDATABASE")
+MYSQL_PORT = int(os.getenv("MYSQLPORT", 3306))# your database name
 MAIL_USER = "ghana19183@gmail.com"
 MAIL_PASS = "jmwe kafu lmdp cmpr"   # Use Gmail App Password (NOT your Gmail login)
 
