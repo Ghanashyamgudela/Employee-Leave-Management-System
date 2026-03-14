a2a116ce20ebf5c99f630f1cfcd33b0d7925921a
Ghanashyamgudela <157698137+Ghanashyamgudela@users.noreply.github.com>
2026-03-14 13:48:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-MYSQL_HOST = 'localhost'       # your MySQL host, usually localhost
-MYSQL_USER = 'root'            # your MySQL username
-MYSQL_PASSWORD = 'Ghana@1230'            # your MySQL password
-MYSQL_DB = 'student'  # your database name
+MYSQLHOST = 'mysql.railway.internal'       # your MySQL host, usually localhost
+MYSQLUSER = 'root'            # your MySQL username
+MYSQLPASSWORD = 'cxVIqcOfPKWrUPZVknOCkOneoyjMkEDF'            # your MySQL password
+MYSQLDATABASE = 'railway'  # your database name
 MAIL_USER = "ghana19183@gmail.com"
 MAIL_PASS = "jmwe kafu lmdp cmpr"   # Use Gmail App Password (NOT your Gmail login)
 
