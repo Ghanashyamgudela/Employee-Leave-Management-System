723194ac91318ee81227d2c94db322b2646c6949
Ghanashyamgudela <157698137+Ghanashyamgudela@users.noreply.github.com>
2026-05-03 20:14:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,12 +30,12 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 
 
-app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "Ghana@1230"
-app.config['MYSQL_DB'] = "student"
-app.config['MYSQL_PORT'] = 3306
 
+app.config['MYSQL_HOST'] = os.environ.get("MYSQLHOST")
+app.config['MYSQL_USER'] = os.environ.get("MYSQLUSER")
+app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQLPASSWORD")
+app.config['MYSQL_DB'] = os.environ.get("MYSQLDATABASE")
+app.config['MYSQL_PORT'] = int(os.environ.get("MYSQLPORT", 3306))
 
 
 
